5894385a90dbf3ed83ce64180dcf5a1450196e20
Willam Woodhead <william.woodhead@lux01.co.uk>
2017-03-19 16:51:58 +0000

    Added day 3

--- src/advent_of_code_2015/core.clj
+++ src/advent_of_code_2015/core.clj
@@ -1,7 +1,8 @@
 (ns advent-of-code-2015.core
   (:use [clojure.pprint :only [pprint]])
   (:require [advent-of-code-2015.day-01 :as day-01]
-            [advent-of-code-2015.day-02 :as day-02])
+            [advent-of-code-2015.day-02 :as day-02]
+            [advent-of-code-2015.day-03 :as day-03])
   (:gen-class))
 
 (defn unimplemented
@@ -11,26 +12,31 @@
 (defn part-1
   [day]
   (-> {1 day-01/part1
-       2 day-02/part-1}      
+       2 day-02/part-1
+       3 day-03/part-1}      
       (get day unimplemented)))
 
 (defn part-2
   [day]
   (-> {1 day-01/part2
-       2 day-02/part-2}
+       2 day-02/part-2
+       3 day-03/part-2}
       (get day unimplemented)))
 
 (defn input
   [day]
   (-> {1 "input/day-01.txt"
-       2 "input/day-02.txt"}
+       2 "input/day-02.txt"
+       3 "input/day-03.txt"}
       (get day)
       (slurp)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [day (Integer. (nth args 0))
+  
+  (let [day (try (Integer/parseInt (nth args 0))
+                 (catch NumberFormatException e 1))
         input-txt (input day)]
     (println "Day " day)
     (println "Part 1:\n")
---
+++ src/advent_of_code_2015/day_03.clj
@@ -0,0 +1,50 @@
+(ns advent-of-code-2015.day-03
+  (:gen-class))
+
+(def init-grid
+  {:houses {{:x 0, :y 0} 1}
+   :posn {:x 0, :y 0}})
+
+
+(defn drop-present
+  [state]
+  (update-in state [:houses (:posn state)] (fnil inc 0)))
+
+(defn move-grid
+  [state direction]
+  (if (not-any? (partial = direction) [\^ \v \< \>]) state
+      (-> state
+          (update :posn
+                  (condp = direction
+                    \^ #(update % :y inc)
+                    \v #(update % :y dec)
+                    \< #(update % :x dec)
+                    \> #(update % :x inc)))
+          (drop-present))))
+
+
+(defn partition-input
+  ([input]
+   (partition-input input [] [] false))
+
+  ([input left right even]
+   (if (empty? input) (list left right)
+       (let [x (first input)
+             xs (rest input)
+             left' (if (not even) (conj left x) left)
+             right' (if even (conj right x) right)]
+         (recur xs left' right' (not even))))))
+
+(defn part-1
+  [input]
+  (count (:houses
+          (reduce move-grid init-grid input))))
+
+(defn part-2
+  [input]
+  (let [[real robot _] (partition-input input)]
+    (count (:houses
+            (reduce move-grid
+                    (assoc (reduce move-grid init-grid real)
+                           :posn {:x 0, :y 0})
+                    robot)))))
